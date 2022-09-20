a909515ef21352d88dd39559d23bc29a75ae70a8
pirminis <pirminis@gmail.com>
2022-09-20 21:34:14 +0300

    Add Tampermonkey script

---
+++ format-eldesalarm-log.user.js
@@ -0,0 +1,89 @@
+// ==UserScript==
+// @name         Format eldesalarm log
+// @namespace    https://github.com/pirminis/format-eldesalarm-log
+// @version      0.0.2
+// @description  Automatically format, sort eldesalarm log
+// @author       pirminis
+// @match        https://gates.eldesalarms.com/en/gatesconfig/settings/getlog/ajax/*/device_id/*/filename/*_device.log/view/*.html
+// @updateURL    https://github.com/pirminis/format-eldesalarm-log/raw/master/format-eldesalarm-log.user.js
+// @downloadURL  https://github.com/pirminis/format-eldesalarm-log/raw/master/format-eldesalarm-log.user.js
+// ==/UserScript==
+
+(function(global) {
+  'use strict';
+
+  const dataSplitPattern = /^(\d{4}\.\d{2}\.\d{2}\s\d{2}:\d{2}:\d{2})\s(.*):(\+\d+)$/;
+
+  let body = document.getElementsByTagName('body')[0];
+  let text = body.innerText;
+  let lines = text.split("\n");
+  let normalizedLines = [];
+  let linesForSorting = [];
+  let content = '';
+
+  for (let i = 0; i < lines.length - 1; i++) {
+    if (i % 2 == 1) {
+      continue;
+    }
+
+    normalizedLines.push(lines[i].toString() + lines[i + 1].toString());
+  }
+
+  for (let i = 0; i < normalizedLines.length; i++) {
+    let match = normalizedLines[i].match(dataSplitPattern);
+
+    linesForSorting.push({ date: match[1], text: match[2], phone: match[3] });
+  }
+
+  linesForSorting.sort(compareDates);
+
+  for (let i = 0; i < linesForSorting.length; i++) {
+    content += `
+      <tr>
+        <td>${linesForSorting[i].date}</td>
+        <td>${linesForSorting[i].text}</td>
+        <td>${linesForSorting[i].phone}</td>
+      </tr>
+    `
+  }
+
+  let output = `
+    <style>
+      table {
+        border-collapse: collapse;
+      }
+      table, th, td {
+        font-family: monospace;
+        border: 1px solid black;
+        padding: 3px 8px;
+      }
+    </style>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Date (UTC)</th>
+          <th>Text</th>
+          <th>Phone number</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${content}
+      </tbody>
+    </table>
+  `;
+
+  body.innerHTML = output;
+
+  function compareDates(a, b) {
+    if (a.date < b.date) {
+      return -1;
+    }
+
+    if (a.date > b.date) {
+      return 1;
+    }
+
+    return 0;
+  }
+})(window);
